6fb8b5d0dee8d3455c45ec73b549e6201eb51d35
jony <nazrul229502@gmail.com>
2025-02-11 23:57:05 +0600

    completed upto navbar

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        fontFamily:{
+          serif : [ 'Poppins', 'serif'],
+        },
+       
+      extend: {},
+    },
+    plugins: [],
+  }
