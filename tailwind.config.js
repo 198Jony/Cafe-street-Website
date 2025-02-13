4055a521ce29066e487bc2716459547f5b67591f
jony <nazrul229502@gmail.com>
2025-02-14 00:20:45 +0600

    completed upto deliver section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@
         fontFamily:{
           serif : [ 'Poppins', 'serif'],
         },
-        
+        backgroundImage: {
+          'hero-pattern': "url('images/Rectangle 2 (4).png')",
+        },
       extend: {},
     },
     plugins: [],
